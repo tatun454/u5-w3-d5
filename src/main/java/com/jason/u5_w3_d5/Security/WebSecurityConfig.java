1b6bda9b1246dde6ce8efd0a1c799783abe4b379
Jason <jasonct454@gmail.com>
2025-10-24 13:14:28 +0200

    Update WebSecurityConfig.java
    aggiunto jwt nel web securityconfig

--- src/main/java/com/jason/u5_w3_d5/Security/WebSecurityConfig.java
+++ src/main/java/com/jason/u5_w3_d5/Security/WebSecurityConfig.java
@@ -3,16 +3,29 @@ package com.jason.u5_w3_d5.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtTokenProvider tokenProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(tokenProvider, customUserDetailsService);
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -20,9 +33,16 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
                 .authorizeHttpRequests(auth -> auth
                         //tutti possoono visualizzano la lista degli eventi disponibili
                         .requestMatchers(HttpMethod.GET, "/api/events").permitAll()
@@ -30,13 +50,14 @@ public class WebSecurityConfig {
 
                         // Gestione eventi per gli organizzatori
                         .requestMatchers("/api/organizer/events/**").hasAuthority("ORGANIZZATORE_EVENTI")
-
                         // Prenotazione e visualizzazione/annullamento per utenti normali
                         .requestMatchers("/api/bookings/**").hasAuthority("UTENTE_NORMALE")
 
-                        .anyRequest().authenticated() // ovviamente tutto questo richiede autenticazione
-                )
-                .httpBasic(withDefaults());
+                        .anyRequest().authenticated()
+                );
+
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
