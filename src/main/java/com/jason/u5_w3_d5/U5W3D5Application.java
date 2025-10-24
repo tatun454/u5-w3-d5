c9366f6c6130ab86f6611b5eabd2f7d4dd66f059
Jason <jasonct454@gmail.com>
2025-10-24 09:51:19 +0200

    base

---
+++ src/main/java/com/jason/u5_w3_d5/U5W3D5Application.java
@@ -0,0 +1,13 @@
+package com.jason.u5_w3_d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jason/u5_w3_d5/U5W3D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.u5_w3_d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
